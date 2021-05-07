aac92351693b8484aa05d1b65e97e08ab762b7b8
PedroJungers <pedrojungers@gmail.com>
2021-05-06 22:52:37 -0300

    codigo.cpp Atualizado

--- codigo.cpp
+++ codigo.cpp
@@ -24,4 +24,84 @@ int main(){
 	}else if(opcao == 2){
 		cout<<endl<<"Você selecionou a opção "<< opcao<<": Retângulo"<<endl<<endl;
 	}
+
+    if(opcao == 1){
+		
+		cout<<"O que você deseja realizar com a Circunferência?"<<endl<<endl<<"1 - Calcular Área"<<endl<<"2 - Calcular Perímetro"<<endl<<endl<<"Digite a opção: ";
+		cin >>opcaoR;
+		while(opcaoR < 1 or opcaoR > 2)
+		{
+			cout<<endl<<"Seleciona uma opção valida: ";
+			cin >> opcaoR;
+		}
+		
+			if(opcaoR == 1 ){
+				
+				cout<<endl<<"Você escolheu calcular a Área da Circunferência"<<endl<<endl;
+				
+				cout<<"Digite o valor do raio da circunferência: ";
+				cin>> raio;
+			
+				area = pi*(raio*raio);
+			
+				cout<<endl<<"A Área da circunferência é: "<<area<<endl<<endl;
+		
+			
+			}else if(opcaoR == 2){
+				
+				cout<<endl<<"Você escolheu calcular o Perímetro da Circunferência"<<endl<<endl;
+			
+				cout<<"Digite o valor do raio da circunferência: ";
+				cin>> raio;
+			
+				perimetro = 2*pi*raio;
+			
+				cout<<endl<<"O perímetro da Circunfêrencia é: "<<perimetro<<endl<<endl;
+			
+			}
+			
+			
+		
+	} else if(opcao == 2){
+		
+			cout<<"O que você deseja realizar com o Retângulo?"<<endl<<endl<<"1 - Calcular Área"<<endl<<"2 - Calcular Perímetro"<<endl<<endl<<"Digite a opção: ";
+			cin >>opcaoR;
+			while(opcaoR < 1 or opcaoR > 2)
+			{
+				cout<<endl<<"Seleciona uma opção valida: ";
+				cin >> opcaoR;
+			}
+		
+				if(opcaoR == 1 ){
+				
+					cout<<endl<<"Você escolheu calcular a Área do Retângulo"<<endl<<endl;
+				
+					cout<<"Digite o valor da Base do Retângulo: ";
+					cin>> base;
+					cout<<"Digite o valor da Altura do Retângulo: ";
+					cin>> altura;
+			
+					area = base*altura;
+			
+					cout<<endl<<"A Área do Retângulo é: "<<area<<endl<<endl;
+		
+			
+				}else if(opcaoR == 2){
+				
+					cout<<endl<<"Você escolheu calcular o Perímetro do Retângulo"<<endl<<endl;
+			
+					cout<<"Digite o valor da Base do Retângulo: ";
+					cin>> base;
+					cout<<"Digite o valor da Altura do Retângulo: ";
+					cin>> altura;
+			
+					perimetro = 2*(base+altura);
+			
+					cout<<endl<<"O perímetro do Retângulo é: "<<perimetro<<endl<<endl;
+			
+				}
+	}
+	
+	
+	return 0;
 }
